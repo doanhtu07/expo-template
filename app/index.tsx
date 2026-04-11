a203013a6a249b3f5a75ab7aa43011925b39f883
Anh Tu Do <doanhtu07@gmail.com>
2026-04-10 21:10:17 -0500

    Add expo + docs + scripts for maintenance

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Index() {
+  return (
+    <View style={styles.root}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
